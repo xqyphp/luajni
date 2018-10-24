52e5d15a33d4a448c3b1cd0abc15e6e5bda5e082
xqyphp <xqyphp@qq.com>
2018-10-24 10:10:22 +0800

    添加一个例子

---
+++ test.lua
@@ -0,0 +1,23 @@
+function print_service()
+	--需要在 native 程调用 luajni_push_object 存储
+    local app = luajni.getJavaObject("application"); --global
+
+    local activity = luajni.callMethod(app, "getSystemService",
+        "(Ljava/lang/String;)Ljava/lang/Object;", "activity")
+
+
+    local services = luajni.callMethod(activity, "getRunningServices",
+        "(I)Ljava/util/List;", 300)
+
+    local size = luajni.callMethod(services, "size", "()I");
+
+
+    for i = 0, size - 1 do
+        local info = luajni.callMethod(services, "get", "(I)Ljava/lang/Object;", i)
+        local strproc = luajni.getField(info, "process",
+            "Ljava/lang/String;")
+        luajni.deleteLocalRef(info)
+    end
+    luajni.deleteLocalRef(activity)
+    luajni.deleteLocalRef(services)
+end
